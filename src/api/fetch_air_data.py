30d9f1ca2a999420cd502ed08f9f4b535c483480
Uroosham Memon <158079613+UrooshamMemon@users.noreply.github.com>
2026-08-30 05:24:57 +0000

    updated fetch air data

--- src/api/fetch_air_data.py
+++ src/api/fetch_air_data.py
@@ -1,44 +1,55 @@
 import requests
-from src.config import latitude, longitude, api_key, air_data_url
+
+from src.config import (
+    latitude,
+    longitude,
+    historical_air_url
+)
 
 def fetch_air_data():
 
     params = {
-        "lat" : latitude,
-        "lon" : longitude,
-        "appid" : api_key
+        "latitude": latitude,
+        "longitude": longitude,
+        "hourly": "us_aqi,pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone",
+        "forecast_days": 1
     }
 
-    response = requests.get(air_data_url, params=params, timeout=10)
+    response = requests.get(
+        historical_air_url,
+        params=params,
+        timeout=10
+    )
 
     try:
-        response.raise_for_status()   
+        response.raise_for_status()
+
         data = response.json()
-        
-        main_data = data["list"][0].get('main')
-        components = data["list"][0].get('components')
+        hourly_data = data["hourly"]
 
         air_data = {
-            "aqi" : main_data.get('aqi'),
-            "pm10" : components.get('pm10'),
-            "pm2.5" : components.get('pm2_5'),
-            "co" : components.get('co'),
-            "no" : components.get('no'),
-            "no2" : components.get('no2'),
-            "o3" : components.get('o3'),
-            "so2" : components.get('so2'),
-            "nh3" : components.get('nh3')
+            "aqi": hourly_data["us_aqi"][-1],
+            "pm10": hourly_data["pm10"][-1],
+            "pm2.5": hourly_data["pm2_5"][-1],
+            "co": hourly_data["carbon_monoxide"][-1],
+            "no2": hourly_data["nitrogen_dioxide"][-1],
+            "o3": hourly_data["ozone"][-1],
+            "so2": hourly_data["sulphur_dioxide"][-1],
+            "nh3": None
         }
+
         return air_data
-        
-    except requests.exceptions.Timeout:
-        print("Timeout")
 
     except requests.exceptions.ConnectionError:
         print("Connection Error")
 
+    except requests.exceptions.Timeout:
+        print("Timeout")
+
     except requests.exceptions.HTTPError as e:
-             print(f"HTTP Error: {e}")
-    
+        print(f"HTTP Error: {e}")
+
     except requests.exceptions.RequestException as e:
-        print(f"Request Error: {e}")  
+        print(f"Request Error: {e}")
+
+    return None
